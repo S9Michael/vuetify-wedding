17a747e1ee962cd85695a8ea0cf5f962592672b9
Michael Sulc <sulcmisak@gmail.com>
2025-05-28 21:47:58 +0200

    Hide upper bar before login

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,49 @@
 <template>
   <v-app>
-    <v-main>  
-      <v-app-bar 
-      scroll-behavior="hide"      
-      title = "" 
-      text-color="#5A86AD"       
+    <v-main>
+      <!-- App Bar, zobrazí se jen na některých stránkách -->
+      <v-app-bar
+        v-if="showAppBar"
+        scroll-behavior="hide"
+        title=""
+        text-color="#5A86AD"
       >
-      <v-switch @click="toggleTheme" 
-                v-model="model" 
-                color=#5A86AD  
-                hide-details="true"    
-                
-                inset
-       /> 
-       <v-btn width="150px" to="/home">ÚVOD</v-btn>
-      <v-btn width="150px" to="/about">MY DVA</v-btn>
-      <v-btn width="150px" to="/wedding">NAŠE SVATBA</v-btn>
-      <v-btn width="150px" to="/schedule">HARMONOGRAM</v-btn>
-      <v-btn width="200px" to="/info">UŽITEČNÉ INFORMACE</v-btn>
-  
+        <v-switch
+          @click="toggleTheme"
+          v-model="model"
+          color="#5A86AD"
+          hide-details="true"
+          inset
+        />
+        <v-btn width="150px" to="/home">ÚVOD</v-btn>
+        <v-btn width="150px" to="/about">MY DVA</v-btn>
+        <v-btn width="150px" to="/wedding">NAŠE SVATBA</v-btn>
+        <v-btn width="150px" to="/schedule">HARMONOGRAM</v-btn>
+        <v-btn width="200px" to="/info">UŽITEČNÉ INFORMACE</v-btn>
       </v-app-bar>
-      
-      <router-view/>
+
+      <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import { useTheme } from 'vuetify'
+import { useRoute } from 'vue-router'
 
 const theme = useTheme()
-const model = ref(theme.global.name.value) // Sync with v-model
+const model = ref(theme.global.name.value)
 
-function toggleTheme () {
+function toggleTheme() {
   theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
 }
+
+// --- Podmíněné zobrazení app bar ---
+const route = useRoute()
+const showAppBar = computed(() => {
+  // Cesty, kde se app bar nezobrazuje
+  const hiddenPaths = ['/','/forgettenpassword'] 
+  return !hiddenPaths.includes(route.path)
+})
 </script>
-
-
-
